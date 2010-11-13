4aae9163fc970f7e8ce40d51e257c2db7fca0052
Matthew J Morrison <mattj.morrison@gmail.com>
2010-11-12 21:57:52 -0600

    more erlang coding

--- useless.erl
+++ useless.erl
@@ -1,7 +1,9 @@
 -module(useless).
 -author("Matthew J. Morrison").
 
--export([add/2, hello/0, greet_and_add_two/1, greet/2, head/1, second/1, same/2, valid_time/1, driving_age/1, oh_good/1, help_me/1]).
+-export([add/2, hello/0, greet_and_add_two/1, greet/2, head/1, second/1, 
+		same/2, valid_time/1, driving_age/1, oh_good/1, help_me/1,
+		prepend/2, beach/1]).
 
 add(A,B) ->
 	A + B.
@@ -71,3 +73,27 @@ help_me(Animal) ->
 			  true -> "fgdadfgna"
 		   end,
 	{Animal, "says " ++ Talk ++ "!"}.
+	
+	
+prepend(X, []) ->
+	[X];
+prepend(X,Set) ->
+	case lists:member(X, Set) of
+		true -> Set;
+		false -> [X|Set]
+	end.
+	
+	
+beach(Temp) ->
+	case Temp of
+		{celsius, N} when N >= 20, N =< 45 ->
+			'favorable';
+		{kelvin, N} when N >= 293, N =< 318 ->
+			'scientifically favorable';
+		{fahrenheit, N} when N >= 68, N =< 113 ->
+			'favorable in the US';
+		_ ->
+			'avoid beach'
+	end.
+	
+	
