53151dd2258bfebaaa004aada3e4e7e423dc4aec
Matthew J Morrison <mattj.morrison@gmail.com>
2010-11-13 14:43:44 -0600

    stopping at Maps, filters, folds and more

--- hhfuns.erl
+++ hhfuns.erl
@@ -18,3 +18,15 @@ map(F, [H|T]) -> [F(H)|map(F,T)].
 
 incr(X) -> X + 1.
 decr(X) -> X - 1.
+
+base(A) ->
+	B = A + 1,
+	F = fun() -> A * B end,
+	F().
+	
+a() ->
+	Secret = "pony",
+	fun() -> Secret end.
+	
+b(F) ->
+	"a/0's password is " ++ F().
